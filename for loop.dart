b0eb8bcbdf1f374d9d4e0c987a4d3074314e5120
malikinzimam <malikinzimam840@gmail.com>
2024-12-22 22:14:51 +0500

    for and for in

---
+++ for loop.dart
@@ -0,0 +1,29 @@
+// // // void main() {
+// // //   int number = 2;
+// // //   for (int a = 1; a <= 10; a++) {
+// // //     print("$number x $a = ${a*number}");
+// // //   }
+// // // }
+// // void main() {
+// //   List data = ["bilal", "ali", "umer", "inzimam"];
+// //   for (var i = 0; i < data.length; i++) {
+// //     print(data[i]);
+// //   }
+// // }
+// //
+
+// // 
+// void main() {
+//   List number = [1, 2, 3, 4, 5, 6, 77, 8, 9, 10, 22, 12, 15];
+//   List evennumber = [];
+//   List oddnumber = [];
+//   for (var i = 0; i < number.length; i++) {
+//     if (number[i] % 2 == 0) {
+//       evennumber.add(number[i]);
+//     } else {
+//       oddnumber.add(number[i]);
+//     }
+//   }
+//   print(evennumber);
+//   print(oddnumber);
+// }
