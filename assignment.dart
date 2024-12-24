37edb303d8f8e839ed5960ffe99f6bee7a74aa6c
malikinzimam <malikinzimam840@gmail.com>
2024-12-24 12:11:20 +0500

    for and for in

--- assignment.dart
+++ assignment.dart
@@ -1,65 +1,99 @@
+// // // // // // // // // // // // void main() {
+// // // // // // // // // // // //   List number = [10, 20, 30, 40, 50];
+// // // // // // // // // // // //   for (var i = 0; i < number.length; i++) {
+// // // // // // // // // // // //     print(number[i]);
+// // // // // // // // // // // //   }
+// // // // // // // // // // // // }
 // // // // // // // // // void main() {
-// // // // // // // // //   List number = [10, 20, 30, 40, 50];
+// // // // // // // // //   List<int> number = [10, 20, 30, 40, 50];
+// // // // // // // // //   int sum = 0;
 // // // // // // // // //   for (var i = 0; i < number.length; i++) {
-// // // // // // // // //     print(number[i]);
+// // // // // // // // //     sum += number[i];
 // // // // // // // // //   }
+// // // // // // // // //   print(sum);
 // // // // // // // // // }
+// // // // // // // // // // void main() {
+// // // // // // // // // //   List<int> numbers = [1, 2, 3, 4, 5];
+// // // // // // // // // //   int sum = 0;
+
+// // // // // // // // // //   for (int i = 0; i < numbers.length; i++) {
+// // // // // // // // // //     sum += numbers[i];
+// // // // // // // // // //   }
+
+// // // // // // // // // //   print('Total Sum: $sum');
+// // // // // // // // // // }
+// // // // // // // // void main() {
+// // // // // // // //   List names = ["ali", "umer", "inzimam", "israr"];
+// // // // // // // //   final reversed = names.reversed;
+// // // // // // // //   for (var i in reversed) {
+// // // // // // // //     print(i);
+// // // // // // // //   }
+// // // // // // // // }
+// // // // // // // void main() {
+// // // // // // //   List names = ["ali", "umer", "inzimam", "israr"];
+// // // // // // //   for (var i = names.length - 1; i >= 0; i--) {
+// // // // // // //     print(names[i]);
+// // // // // // //   }
+// // // // // // // }
 // // // // // // void main() {
-// // // // // //   List<int> number = [10, 20, 30, 40, 50];
-// // // // // //   int sum = 0;
-// // // // // //   for (var i = 0; i < number.length; i++) {
-// // // // // //     sum += number[i];
+// // // // // //   var sum = 0;
+// // // // // //   for (var i = 1; i <= 100; i++) {
+// // // // // //     if (i % 2 == 0) {
+// // // // // //       sum += i;
+// // // // // //     }
 // // // // // //   }
 // // // // // //   print(sum);
 // // // // // // }
-// // // // // // // void main() {
-// // // // // // //   List<int> numbers = [1, 2, 3, 4, 5];
-// // // // // // //   int sum = 0;
-
-// // // // // // //   for (int i = 0; i < numbers.length; i++) {
-// // // // // // //     sum += numbers[i];
-// // // // // // //   }
-
-// // // // // // //   print('Total Sum: $sum');
-// // // // // // // }
 // // // // // void main() {
-// // // // //   List names = ["ali", "umer", "inzimam", "israr"];
-// // // // //   final reversed = names.reversed;
-// // // // //   for (var i in reversed) {
-// // // // //     print(i);
+// // // // //   List word = ["ali", "inzimam", "umer", "israr"];
+// // // // //   for (var i = 0; i < word.length; i++) {
+// // // // //     print('${word[i]} : ${word[i].length}');
 // // // // //   }
 // // // // // }
 // // // // void main() {
-// // // //   List names = ["ali", "umer", "inzimam", "israr"];
-// // // //   for (var i = names.length - 1; i >= 0; i--) {
-// // // //     print(names[i]);
+// // // //   List names = ["ali", "umer", "inzimam", "bilal"];
+// // // //   for (var i in names) {
+// // // //     print('${i} : ${i.length}');
 // // // //   }
 // // // // }
 // // // void main() {
-// // //   var sum = 0;
-// // //   for (var i = 1; i <= 100; i++) {
-// // //     if (i % 2 == 0) {
-// // //       sum += i;
-// // //     }
+// // //   int sum = 1;
+// // //   for (var i = 1; i <= 10; i++) {
+// // //     sum *= i;
 // // //   }
 // // //   print(sum);
 // // // }
+
 // // void main() {
-// //   List word = ["ali", "inzimam", "umer", "israr"];
-// //   for (var i = 0; i < word.length; i++) {
-// //     print('${word[i]} : ${word[i].length}');
+// //   List<int> number = [10, 20, 30, 40, 50];
+// //   int sum = 0;
+// //   for (int i = 0; i < number.length; i++) {
+// //     sum += number[i];
 // //   }
+// //   double avareg = sum / number.length;
+// //   print("avareg is : $avareg");
 // // }
 // void main() {
-//   List names = ["ali", "umer", "inzimam", "bilal"];
-//   for (var i in names) {
-//     print('${i} : ${i.length}');
+//   List<int> number = [
+//     10,
+//     20,
+//     30,
+//     40,
+//     50,
+//   ];
+//   int sum = 0;
+//   for (var i in number) {
+//     sum += i;
 //   }
+//   double avareg = sum / number.length;
+//   print(avareg);
 // }
 void main() {
-  int sum = 1;
   for (var i = 1; i <= 10; i++) {
-    sum *= i;
+    int factorial = 1;
+    for (var j = 1; j <= i; j++) {
+      factorial *= j;
+    }
+    print('Factorial of ${i}: ${factorial}');
   }
-  print(sum);
 }
